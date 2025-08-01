4c0169cdfd34b6f31a557f0e0430d4a46c2d3c87
Bhavik Gabani <sprojects112@gmail.com>
2025-08-01 13:54:41 +0530

    update packages

--- Api/MyDrReferral.Api/Mapper/MappingProfile.cs
+++ Api/MyDrReferral.Api/Mapper/MappingProfile.cs
@@ -6,14 +6,14 @@ using MyDrReferral.Service.Models;
 
 namespace MyDrReferral.Api.Mapper
 {
-    public class MappingProfile:Profile
+    public class MappingProfile : Profile
     {
         public MappingProfile()
         {
             CreateMap<UserRegistrationRequest, ApplicationUser>();
             //.ForMember(usr=>usr.Id,opt=>opt.MapFrom(src=>Guid.NewGuid().ToString()));
 
-            CreateMap<UserLoginRequest,LoginModel>();
+            CreateMap<UserLoginRequest, LoginModel>();
 
             //Convert Service Model to Db type
             //CreateMap<ErrorLogModel, ErrorLogs>();
@@ -32,7 +32,7 @@ namespace MyDrReferral.Api.Mapper
 
             CreateMap<UpdateUserRequestModel, UpdateUserData>();
             CreateMap<ConnectionResponseModel, ConnectionRequestResponse>();
-            CreateMap<RefferModel,Reffer>();
+            CreateMap<RefferModel, Reffer>();
             CreateMap<UpdateRefferStatusModel, Reffer>();
             CreateMap<ConnectionModel, Connection>();
 
